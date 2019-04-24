5d4981d340e3259c86ee85a46065704e1e71ae51
walksa <miwanishioka701@gmail.com>
2019-04-24 23:30:07 +0800

    [modified] stub to arr

--- content.js
+++ content.js
@@ -9,17 +9,16 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   $('label[for="1496973637295144.0"]').click()
 
   const btn = document.getElementsByClassName("_271k _271m _1qjd _7tvm _7tv2 _7tv4")[0]
-  btn.click()
-  btn.click()
-  btn.click()
-  $('._58al').eq(0).val('111')
-  $('._58al').eq(1).val('222')
-  $('._58al').eq(2).val('333')
-  $('._58al').eq(3).val('444')
-  $('._58al').eq(4).val('555')
-  $('._58al').eq(5).val('666')
-  $('._58al').eq(6).val('777')
-  $('._58al').eq(7).val('888')
+  const ads = ['111', '222', '333', '444', '555','666', '777', '888', '999']
+  const ad_rows = ads.length - 5
+  for (let i = 0; i < ad_rows; i++) {
+    btn.click()
+  }
+
+  ads.forEach((item, index) => {
+    $('._58al').eq(index).val(item)
+  })
+
 
 
   const btn2 = document.getElementsByClassName("_271k _271m _1qjd _7tvm _7tv2 _7tv4")[1]
